64f9f3f8847b84263c16b2f706bcd35a0505fd34
Monica Oguntona <Monica.Oguntona@etranzact.com>
2026-09-10 14:06:17 +0100

    New changes

--- tests/bing.spec.js
+++
@@ -1,7 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('open bing homepage', async ({page}) => {
-    await page.goto('https://www.bing.com/');
-
-    await expect(page).toHaveTitle(/Bing/);
-}); 
